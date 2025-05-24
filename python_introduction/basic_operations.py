66cc47c9756be7d0c85221816170ec04c564fefd
yirubal <yiruubal@gmail.com>
2025-05-24 07:45:14 +0300

    add basic python arthimetic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+Sum = number1 + number2
+Difference = number1 - number2
+Product = number1 * number2
+
+print(f'Addition of {number1} and {number2} is {Sum}')
+print(f'Difference of {number1} and {number2} is {Difference}')
+print(f'Multiplication of {number1} and {number2} is {Product}')
