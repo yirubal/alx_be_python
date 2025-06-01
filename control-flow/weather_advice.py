ce28a5315be16e3d0182621fe843c723f84e4035
yirubal <yiruubal@gmail.com>
2025-06-01 21:42:37 +0300

    feat: add weather advice script
    Introduce a script to provide clothing recommendations based on weather input. Handles "sunny," "rainy," "cold," and defaults to a generic response for other inputs.

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+question = str(input("What's the weather like today?: "))
+
+if(question == "sunny"):
+    print("Wear a t-shirt and sunglasses.")
+elif(question == "rainy"):
+    print(" Don't forget your umbrella and a raincoat.")
+elif(question == "cold"):
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
