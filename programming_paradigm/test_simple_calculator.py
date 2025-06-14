be30bcf8e726e9c59fdda4fd6cae839d70cde72f
yirubal <yiruubal@gmail.com>
2025-06-14 16:03:16 +0300

    add simple calculator tool

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,44 @@
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up calculator before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, -2), -3)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(-5, 3), -8)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 3), 12)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(-2, -3), 6)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertEqual(self.calc.divide(-9, -3), 3)
+        self.assertAlmostEqual(self.calc.divide(1, 3), 0.3333333333, places=5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            self.calc.divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# Remember to write additional test methods for subtract, multiply, and divide.
