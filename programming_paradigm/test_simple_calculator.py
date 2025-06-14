1324314ee5fbbd7baebd2e305b5e211908590979
yirubal <yiruubal@gmail.com>
2025-06-14 16:15:37 +0300

    add simple calculator tool

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -27,7 +27,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-6, 2), -3)
         self.assertEqual(self.calc.divide(-9, -3), 3)
