9617f445b986fb64243c7fda2d77b986e989e0c2
yirubal <yiruubal@gmail.com>
2025-06-14 13:16:28 +0300

    add simple bank account management tool

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,11 @@
+
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        self.account_balance += amount
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+    def display_balance(self):
+        print(f"Balance: {self.account_balance}")
