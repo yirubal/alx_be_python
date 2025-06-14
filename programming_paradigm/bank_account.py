d19f8724d36f7759f238092484a52231bbdb41a5
yirubal <yiruubal@gmail.com>
2025-06-14 13:22:29 +0300

    add simple bank account management tool

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -7,5 +7,8 @@ class BankAccount:
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
+            print(f"Withdrew: ${amount}")
+        else:
+            print("Insufficient funds.")
     def display_balance(self):
         print(f"Current Balance: {self.account_balance}")
